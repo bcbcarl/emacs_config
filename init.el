2acd25b9f04bc6378d80da4c249a64d4aa6c9471
Carl X. Su <bcbcarl@gmail.com>
2015-09-29 22:21:21 +0800

    Add backup settings

--- init.el
+++ init.el
@@ -65,5 +65,15 @@
 ;; PHP
 (require 'init-php)
 
+;; Backup settings
+(setq
+  backup-by-copying t
+  backup-directory-alist
+    '(("." . "~/.saves"))
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
 (provide 'init)
 ;;; init.el ends here
