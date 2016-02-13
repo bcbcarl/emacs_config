20a638cd535e30027cee06203ca356799201cae1
Carl X. Su <bcbcarl@gmail.com>
2016-02-14 00:22:14 +0800

    Remove evil-mode from CIDER's REPL

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -21,6 +21,9 @@
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 (add-hook 'cider-repl-mode-hook #'my-cider-repl-mode-hook)
 
+(require 'evil)
+(evil-set-initial-state 'cider-repl-mode 'emacs)
+
 (provide 'init-clojure)
 
 ;;; init-clojure.el ends here
